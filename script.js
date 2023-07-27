c5e118869e6c2955c005590ebeaf20ed7cd902a6
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-07-27 17:29:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // Your code here.
+
+    const itemsContainer = document.querySelector(".items");
+    let isDragging = false;
+    let startPositionX;
+    let scrollLeft;
+
+    itemsContainer.addEventListener("mousedown", (e) => {
+      isDragging = true;
+      startPositionX = e.pageX - itemsContainer.offsetLeft;
+      scrollLeft = itemsContainer.scrollLeft;
+      itemsContainer.classList.add("active");
+    });
+
+    itemsContainer.addEventListener("mousemove", (e) => {
+      if (!isDragging) return;
+      e.preventDefault();
+      const x = e.pageX - itemsContainer.offsetLeft;
+      const walk = (x - startPositionX) * 2; // Adjust drag speed here
+      itemsContainer.scrollLeft = scrollLeft - walk;
+    });
+
+    itemsContainer.addEventListener("mouseup", () => {
+      isDragging = false;
+      itemsContainer.classList.remove("active");
+    });
+
+    itemsContainer.addEventListener("mouseleave", () => {
+      isDragging = false;
+      itemsContainer.classList.remove("active");
+    });
+  
